d1dcd01d0f8fc01348025231caa032bb5dd3a04d
Michael Kirsten <kirsten@ira.uka.de>
2014-08-20 18:46:12 +0200

    Specification based on Environment class

--- src/de/uni/trier/infsec/lib/network/NetworkClient.java
+++ src/de/uni/trier/infsec/lib/network/NetworkClient.java
@@ -4,24 +4,48 @@ import de.uni.trier.infsec.environment.Environment;
 
 public class NetworkClient {
 
-	public static /*@ helper @*/ void send(byte[] message, String server, int port) throws NetworkError {
-		// input
-		Environment.untrustedOutput(0x2301);
-		Environment.untrustedOutputMessage(message);
-		Environment.untrustedOutputString(server);
-		Environment.untrustedOutput(port);
-		// output
-		if ( Environment.untrustedInput()==0 ) throw new NetworkError();
-	}
+    /*@ public behaviour
+      @ requires Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ assignable Environment.inputCounter, Environment.result;
+      @ signals_only NetworkError, ArrayIndexOutOfBoundsException, Error;
+      @ diverges true;
+      @ ensures true;
+      @ signals (NetworkError e) Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ signals (ArrayIndexOutOfBoundsException e)
+      @                 Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ signals (Error e) Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @*/
+    public static /*@ helper @*/ void send(byte[] message, String server, int port)
+            throws NetworkError {
+        // input
+        Environment.untrustedOutput(0x2301);
+        Environment.untrustedOutputMessage(message);
+        Environment.untrustedOutputString(server);
+        Environment.untrustedOutput(port);
+        // output
+        if ( Environment.untrustedInput()==0 ) throw new NetworkError();
+    }
 
-	public static byte[] sendRequest(byte[] message, String server, int port) throws NetworkError {
-		// input
-		Environment.untrustedOutput(0x2302);
-		Environment.untrustedOutputMessage(message);
-		Environment.untrustedOutputString(server);
-		Environment.untrustedOutput(port);
-		// output
-		if ( Environment.untrustedInput()==0 ) throw new NetworkError();
-		return Environment.untrustedInputMessage();
-	}
+    /*@ public behaviour
+      @ requires Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ assignable Environment.inputCounter, Environment.result;
+      @ signals_only NetworkError, ArrayIndexOutOfBoundsException, Error;
+      @ diverges true;
+      @ ensures true;
+      @ signals (NetworkError e) Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ signals (ArrayIndexOutOfBoundsException e)
+      @                 Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @ signals (Error e) Environment.inputValues != null && 0 <= Environment.inputCounter;
+      @*/
+    public static /*@ helper @*/ byte[] sendRequest(byte[] message, String server, int port)
+            throws NetworkError {
+        // input
+        Environment.untrustedOutput(0x2302);
+        Environment.untrustedOutputMessage(message);
+        Environment.untrustedOutputString(server);
+        Environment.untrustedOutput(port);
+        // output
+        if ( Environment.untrustedInput()==0 ) throw new NetworkError();
+        return Environment.untrustedInputMessage();
+    }
 }
